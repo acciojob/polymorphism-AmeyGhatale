5f0547abce8811510f74e3b1367d6b424045c9ff
“AmeyGhatale” <ameyghatale2016@gmail.com>
2024-02-09 23:55:42 +0530

    Adding Code

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -7,6 +7,10 @@ public class Main {
             int a = p.product(10, 20);
             int b = p.product(10,20,30);
             double c = p.product(10.0, 10.5);
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
     }
 
      public static class Product
